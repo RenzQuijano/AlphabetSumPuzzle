a5e249e2ab8414641cc43b548fe545a493f5bb78
RenzQuijano <renzmichaelquijano@gmail.com>
2025-04-02 16:27:49 -0400

    Equation Solving Update
    -prints all mappings of letters to digits that make the equation true

--- src/AlphabetSumPuzzle.java
+++ src/AlphabetSumPuzzle.java
@@ -10,9 +10,9 @@ public class AlphabetSumPuzzle {
             String puzzle = keyboard.nextLine();//e.g. THE+BEST+SPOT=SPOTS
             String[] tokens = puzzle.split("[^a-zA-Z]");//e.g. THE, BEST, SPOT, SPOTS
             for(int i = 0; i < tokens.length-1;i++)
-                if(tokens[i].length() != 0)//e.g. operands = [THE, BEST, SPOT]
+                if(!tokens[i].isEmpty())//e.g. operands = [THE, BEST, SPOT]
                     operands.add(tokens[i]);//all tokens but the last one represent the sum operands
-                    result = tokens[tokens.length-1];//last token must be the sum result. e.g. result = SPOTS
+            result = tokens[tokens.length-1];//last token must be the sum result. e.g. result = SPOTS
             int[] digitValues = new int[256];//maps characters to digits
             for(int i = 0; i < 256;i++)//initialize digitValues to -1
                 digitValues[i] = -1;//symbol for empty cell!
@@ -51,16 +51,16 @@ public class AlphabetSumPuzzle {
         boolean complete = true;
         char unassignedVariable = 0;
         for(char variable: variables)
-            if(digitValues[variable] == -1){//varible hasn't been assigned to any value yet!
-                        complete = false;//assinment is not complete
+            if(digitValues[variable] == -1){//variable hasn't been assigned to any value yet!
+                        complete = false;//assignment is not complete
                 unassignedVariable = variable;//variable without assignment is stored in the unassignedVariable...
                 break;
             }
         if(complete) {//base case
-//do not print all mappings
-//print a mapping only if it satisfies the puzzle
-//THE+BEST+SPOT=SPOTS
-//first calculate the Left-hand-side of the equation
+            //do not print all mappings
+            //print a mapping only if it satisfies the puzzle
+            //THE+BEST+SPOT=SPOTS
+            //first calculate the Left-hand-side of the equation
             int leftHandSide = 0;
             for(String operand: operands){
                 leftHandSide += numericalValue(operand, digitValues);
@@ -69,12 +69,14 @@ public class AlphabetSumPuzzle {
             int rightHandSide = numericalValue(result, digitValues);
 //then, proceed with the following code only if LHS = RHS
 //for extra credits, you should also consider more conditions before moving forward with printing the solutions
-            for(char variable: variables)
-                System.out.print(variable + ": " + digitValues[variable] + "| ");
-            System.out.println();
+            if (leftHandSide == rightHandSide) { //prints all true equation mappings
+                for (char variable : variables)
+                    System.out.print(variable + ": " + digitValues[variable] + "| ");
+                System.out.println();
+            }
         }else{//recursive step
             for(int i = 0; i < 10;i++) {
-                digitValues[unassignedVariable] = i;//assign the unassignedVarialbe to one of the possible digits 0,1,..,9
+                digitValues[unassignedVariable] = i;//assign the unassignedVariable to one of the possible digits 0,1,...,9
                 printAllCombinations(digitValues);//recursive call!
             }
             digitValues[unassignedVariable] = -1;//assign the unassignedVariable to -1 again
